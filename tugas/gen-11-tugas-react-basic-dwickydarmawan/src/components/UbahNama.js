49670b12889fbca6308429aa97c383aab47b74b2
dwickydarmawan22 <dwickydarmawan22@gmail.com>
2022-10-20 07:51:10 +0700

    react basic

--- tugas/gen-11-tugas-react-basic-dwickydarmawan/src/components/UbahNama.js
+++ tugas/gen-11-tugas-react-basic-dwickydarmawan/src/components/UbahNama.js
@@ -4,16 +4,18 @@ import Button from "./Button";
 export default function UbahNama(props) {
   const [greet, setGreet] = React.useState("Hallo");
   const [name, setName] = React.useState("Dwicky Darmawan");
+  const [spiritWord, setSpiritWord] = React.useState("");
   return (
     <>
       <h1 className="text-3xl text-center pt-10 font-bold">
-        {greet}, Selamat Pagi {name} !
+        {greet}, Selamat Pagi {name} ! {spiritWord}
       </h1>
       <Button
         buttonName="Ubah Nama"
         handleClick={() => {
           setName("Sobat Mawan");
           setGreet("こんにちは");
+          setSpiritWord("Have a Nice Day");
         }}
       />
     </>
