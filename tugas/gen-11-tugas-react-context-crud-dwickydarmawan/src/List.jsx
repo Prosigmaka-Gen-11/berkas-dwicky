bf8db44b5496ea4e9ddc6d2f735c1df6d2536a53
dwickydarmawan22 <dwickydarmawan22@gmail.com>
2022-11-01 11:08:09 +0700

    tugas context dwicky

--- tugas/gen-11-tugas-react-context-crud-dwickydarmawan/src/List.jsx
+++ tugas/gen-11-tugas-react-context-crud-dwickydarmawan/src/List.jsx
@@ -12,7 +12,7 @@ export default function List() {
 
     useEffect(() => {
         getMovies()
-    }, [contextDariProvider.movies])
+    }, [contextDariProvider.inputData])
     return (
         <>
             <table border="1" width="100%">
